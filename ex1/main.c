355c90e3bb550884ba0f4e1e62d19cfa99048069
Яков Вишневский <47566864+IakovWish@users.noreply.github.com>
2019-10-10 17:39:38 +0300

    Update main.c

--- ex1/main.c
+++ ex1/main.c
@@ -1,1 +1,15 @@
-здесь скоро будет код
+#pragma warning(disable : 4996) //необходимо для использования устаревших функций (scanf)
+#include <stdio.h>
+
+int main(void)
+{
+	/*int a;
+	int b;*/
+	int a, b;
+	printf("Enter a ");
+	scanf ("%d", &a);
+	printf("Enter b ");
+	scanf ("%d", &b);
+	a > b ? printf("a > b") : a < b ? printf("a < b") : printf("a = b");
+	return 0;
+}
