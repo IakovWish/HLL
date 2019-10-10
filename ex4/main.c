94616ee6a94fd55c8327d8b86a61cd87a6793abc
Яков Вишневский <47566864+IakovWish@users.noreply.github.com>
2019-10-11 01:01:01 +0300

    Update main.c

--- ex4/main.c
+++ ex4/main.c
@@ -1,1 +1,35 @@
-здесь скоро будет код
+#include <stdio.h>
+
+#define MAXLINE 1024
+
+int main(void)
+{
+	char line[MAXLINE];
+	char result[MAXLINE];
+
+	char* in_ptr = line;
+	char* out_ptr = result;
+
+	gets(line);
+
+	while (*in_ptr != '\0')
+	{
+		if (*in_ptr == 'p')
+		{
+			*out_ptr++ = '1';
+			*out_ptr++ = '5';
+		}
+		else
+		{
+			*out_ptr++ = *in_ptr;
+		}
+
+		in_ptr++;
+	}
+
+	*out_ptr = '\0';
+
+	puts(result);
+
+	return 0;
+}
