92738ef70a3d428924161ff8c7f62ad4d6321da2
Яков Вишневский <47566864+IakovWish@users.noreply.github.com>
2019-10-11 00:33:06 +0300

    Update main.c

--- ex2/main.c
+++ ex2/main.c
@@ -1,1 +1,36 @@
-здесь скоро будет код
+#include <stdio.h>
+
+#define MAXLINE 1024
+
+int main(void)
+{
+	char str1[MAXLINE];
+	char str2[MAXLINE];
+
+	int i = 0;
+
+	printf("Enter str1\n");
+	gets(str1);
+	printf("Enter str2\n");
+	gets(str2);
+
+	do
+	{
+		i++;
+	} while (str1[i] != '\0' && str2[i] != '\0');
+
+	if (str1[i] == '\0' && str2[i] == '\0')
+	{
+		printf("str1 = str2");
+	}
+	else if (str2[i] == '\0')
+	{
+		printf("str1 > str2");
+	}
+	else if (str1[i] == '\0')
+	{
+		printf("str1 < str2");
+	}
+
+	return 0;
+}
